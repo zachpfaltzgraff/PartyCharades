ab5f58c1580fc53c1182f43ec1477bed7507e7a8
Zach Pfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2026-07-17 18:08:41 -0400

    initialized dart

---
+++ frontend/bin/frontend.dart
@@ -0,0 +1,5 @@
+import 'package:frontend/frontend.dart' as frontend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${frontend.calculate()}!');
+}
---
+++ frontend/test/frontend_test.dart
@@ -0,0 +1,8 @@
+import 'package:frontend/frontend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
