4a7796e552dfac71974315bff10aa3e906bed2d1
Zach Pfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2026-07-17 18:25:24 -0400

    made base stuff

---
+++ party_charades/lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
