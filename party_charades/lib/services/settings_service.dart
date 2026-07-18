cfb18655dc897e777cc90dfe15a7b7764b282398
Zach Pfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2026-07-17 20:12:30 -0400

    added some new settings

--- party_charades/lib/services/settings_service.dart
+++ party_charades/lib/services/settings_service.dart
@@ -2,8 +2,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
   static const _roundLengthKey = 'round_length';
+  static const _audioKey = 'audio';
+  static const _hapticKey = 'haptic';
 
   static const int defaultRoundLength = 60;
+  static const bool defaultAudio = true;
+  static const bool defaultHaptic = true;
 
   static Future<int> getRoundLength() async {
     final prefs = await SharedPreferences.getInstance();
@@ -15,5 +19,29 @@ class SettingsService {
     final prefs = await SharedPreferences.getInstance();
 
     await prefs.setInt(_roundLengthKey, seconds);
+  }
+
+  static Future<bool> getAudio() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getBool(_audioKey) ?? defaultAudio;
+  }
+
+  static Future<void> setAudio(bool audio) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(_audioKey, audio);
+  }
+
+  static Future<bool> getHaptic() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getBool(_hapticKey) ?? defaultHaptic;
+  }
+
+  static Future<void> setHaptic(bool audio) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(_hapticKey, audio);
   }
 }
