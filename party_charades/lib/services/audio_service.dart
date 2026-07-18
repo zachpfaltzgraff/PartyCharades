a686cb45b72e5aa7cc92f80b7c9270ae1fb8352a
Zach Pfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2026-07-17 20:15:28 -0400

    now not playing audio and haptic respectively

--- party_charades/lib/services/audio_service.dart
+++ party_charades/lib/services/audio_service.dart
@@ -1,19 +1,36 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/services.dart';
+import 'package:party_charades/services/settings_service.dart';
 
 class AudioService {
   final AudioPlayer audioPlayer = AudioPlayer();
 
   Future<void> playCorrect() async {
+    playHapticCorrect();
+    if(!await SettingsService.getAudio()) return;
+
     await audioPlayer.stop();
     await audioPlayer.play(AssetSource('sounds/correct.mp3'));
+  }
+
+  Future<void> playWrong() async {
+    playHapticWrong();
+    if(!await SettingsService.getAudio()) return;
+
+    await audioPlayer.stop();
+    await audioPlayer.play(AssetSource('sounds/wrong.mp3'));
+
+    HapticFeedback.heavyImpact();
+  }
+
+  Future<void> playHapticCorrect() async {
+    if(!await SettingsService.getHaptic()) return;
 
     HapticFeedback.mediumImpact();
   }
 
-  Future<void> playWrong() async {
-    await audioPlayer.stop();
-    await audioPlayer.play(AssetSource('sounds/wrong.mp3'));
+  Future<void> playHapticWrong() async {
+    if(!await SettingsService.getHaptic()) return;
 
     HapticFeedback.heavyImpact();
   }
